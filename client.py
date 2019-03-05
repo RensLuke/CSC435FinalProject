1a829425e92fecb2eb72c43e25b12cf00ed35c01
LukeRens-Desktop <rensld@jacks.sfasu.edu>
2019-03-05 17:37:39 -0600

    committing updated client (just a test)

--- client.py
+++ client.py
@@ -1,38 +1,43 @@
+import cv2
+import mss
+import numpy
 import socket
-import random
 
-# using loop back address and port 5000
-host = "144.96.33.160"
-port = 80
 
-# initializing socket object
-s = socket.socket()
+def screenrecord():
+    host = "127.0.0.1"
+    port = 5000
+    s = socket.socket()
+    s.connect((host, port))
 
-# binding loop back address and port to socket
-s.connect((host, port))
+    with mss.mss() as sct:
+        # Part of the screen to capture
+        monitor = {"top": 40, "left": 0, "width": 800, "height": 640}
 
-# generating random number, casting it to string (int cant be decoded, only string can)
-message = str(random.randint(1, 10000))
+        while "Screen capturing":
 
-while message != 'n':
-    # sending encoded number to server
-    print("sending number to server: ", message)
-    s.send(message.encode('utf-8'))
+            # Get raw pixels from the screen, save it to a Numpy array
+            img = numpy.array(sct.grab(monitor))
+            hold = str(img.tostring())
+            s.send(hold.encode('utf-8'))
+            # Display the picture
+            # cv2.imshow("OpenCV/Numpy normal", img)
 
-    # receiving acknowledgement
+            # Press "q" to quit
+            if cv2.waitKey(25) & 0xFF == ord("q"):
+                cv2.destroyAllWindows()
+                break
+
+
+def senddata():
+    host = "144.96.33.160"
+    port = 80
+    s = socket.socket()
+    s.connect((host, port))
+
     data = s.recv(1024).decode('utf-8')
-    print("Received from server: '", data, "' ")
 
-    # Prompting user if they would like to send another number
-    decision = input("Would you like to send another number? (y/n): ")
-    if decision == 'y':
-        # send number
-        message = str(random.randint(1, 10000))
-        print("")
-    else:
-        message = "n"
 
-# terminate connection
-s.close()
+screenrecord()
 
 
