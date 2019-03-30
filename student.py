12e1a405283dbe49aa39b68cca36f36c8b5ff866
LukeRens-Desktop <rensld@jacks.sfasu.edu>
2019-03-30 14:30:48 -0500

    branch updated, moved logic from student and teacher into socketbuilder class so you can create a client or server connection from any function/class. error checking also added

--- student.py
+++ student.py
@@ -1,53 +1,13 @@
-from socket import socket
-from zlib import decompress
 from SocketBuilder import SocketObj
-import pygame
+from threading import Thread
 
 WIDTH = 1920
 HEIGHT = 1080
 
 
-def recvall(conn, length):
-    """ Retreive all pixels. """
-
-    buf = b''
-    while len(buf) < length:
-        data = conn.recv(length - len(buf))
-        if not data:
-            return data
-        buf += data
-    return buf
-
-
 def main():
     new_socket = SocketObj('127.0.0.1', 5000, 'client')
-    pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    clock = pygame.time.Clock()
-    watching = True
-    new_socket.sock.connect((new_socket.host, new_socket.port))
-    try:
-        while watching:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    watching = False
-                    break
-
-            # Retrieve the size of the pixels length
-            size_len = int.from_bytes(new_socket.sock.recv(1), byteorder='big')
-            # Retrieve the pixels length
-            PixelLength = int.from_bytes(new_socket.sock.recv(size_len), byteorder='big')
-            # Retrieve the pixels
-            pixels = decompress(recvall(new_socket.sock, PixelLength))
-            # Create the Surface from raw pixels
-            img = pygame.image.fromstring(pixels, (WIDTH, HEIGHT), 'RGB')
-
-            # Display the picture
-            screen.blit(img, (0, 0))
-            pygame.display.flip()
-            clock.tick(60)
-    finally:
-        new_socket.sock.close()
+    Thread(target=new_socket.client_connection(1920, 1080)).start()
 
 
 if __name__ == '__main__':
--- teacher.py
+++ teacher.py
@@ -1,23 +1,14 @@
-from screenshot import ScreenShotObj
 from SocketBuilder import SocketObj
-from SendScreenShots import SendScreenShotObj
 from threading import Thread
 
 
-def main():
+def establish_connection():
     new_socket = SocketObj('127.0.0.1', 5000, 'server')
-    try:
-        new_socket.sock.listen(5)
-        print('Server started.')
-        while 'connected':
-            client_conn, client_addr = new_socket.sock.accept()
-            print('Client connected IP:', client_addr)
-            SS = ScreenShotObj()
-            new_video_stream = SendScreenShotObj(client_conn, SS)
-            thread = Thread(target=new_video_stream.send())
-            thread.start()
-    finally:
-        sock.close()
+    Thread(target=new_socket.server_connection()).start()
+
+
+def main():
+    establish_connection()
 
 
 if __name__ == '__main__':
