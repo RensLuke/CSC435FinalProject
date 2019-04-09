ff8f5a5da6fb55e7677fa74cbfef48ae14b1ac27
LukeRens-Desktop <rensld@jacks.sfasu.edu>
2019-04-09 14:57:26 -0500

    got them sons of bitches working finally

--- doesthiswork.py
+++ doesthiswork.py
@@ -1,7 +1,7 @@
 from zlib import decompress
 import socket
 from screenshot import ScreenShotObj
-import pygame
+
 
 UDP_IP_ADDRESS = "127.0.0.1"
 UDP_PORT_NO = 6789
@@ -11,48 +11,49 @@ HEIGHT = 1080
 serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
-
-data, addr = serverSock.recvfrom(32)
-i = 0
-chunk = b''
-num = data.decode('utf-8')
-print(num, '\n')
 while True:
-    print(i)
-    i += 1
-    data1, addr1 = serverSock.recvfrom(64000)
-    chunk = chunk + data1
-    if i == int(num)-1:
-        print("did it get here")
-        break
+    print("Connected and ready")
+    try:
+        data, addr = serverSock.recvfrom(32)
+    except OSError:
+        print('bad receive')
+    else:
+        chunk = b''
+        num = data.decode('utf-8')
 
-with open('test.txt', 'rb') as filereader:
-    display = filereader.read()
-if display == chunk:
-    print('They are the same')
+        for j in range(int(num)):
+            data, addr = serverSock.recvfrom(64000)
+            chunk = chunk + data
 
-pygame.init()
-pygame.display.set_caption('Rolt VNC')
-screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
-clock = pygame.time.Clock()
-watching = True
+        with open('test.txt', 'rb') as binary_file:
+            OGchunk = binary_file.read()
 
+        if chunk == OGchunk:
+            print("They are the same")
 
-while watching:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            watching = False
-            break
-
-    pixels = decompress(display)
-    # Create the Surface from raw pixels
-    img = pygame.image.fromstring(pixels, (WIDTH, HEIGHT), 'RGB')
-
-    # Display the picture
-    screen.blit(img, (0, 0))
-    pygame.display.flip()
-    clock.tick(60)
-
+    '''
+    pygame.init()
+    pygame.display.set_caption('Rolt VNC')
+    screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+    clock = pygame.time.Clock()
+    watching = True
+    
+    while watching:
+        print("here")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                watching = False
+                break
+    
+        pixels = decompress(chunk)
+        # Create the Surface from raw pixels
+        img = pygame.image.fromstring(pixels, (WIDTH, HEIGHT), 'RGB')
+    
+        # Display the picture
+        screen.blit(img, (0, 0))
+        pygame.display.flip()
+        clock.tick(60)
+    '''
 
 
 
