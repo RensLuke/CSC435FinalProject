aa708eb4602d1750777cf54b749711a1f555f1ad
LukeRens-Desktop <rensld@jacks.sfasu.edu>
2019-04-09 19:12:42 -0500

    got them sons of bitches working finally

--- doesthiswork.py
+++ doesthiswork.py
@@ -1,7 +1,7 @@
-from zlib import decompress
+import zlib
 import socket
 from screenshot import ScreenShotObj
-
+import pygame
 
 UDP_IP_ADDRESS = "127.0.0.1"
 UDP_PORT_NO = 6789
@@ -11,50 +11,57 @@ HEIGHT = 1080
 serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
 
+pygame.init()
+pygame.display.set_caption('Rolt VNC')
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+clock = pygame.time.Clock()
+watching = True
+decompressCounter = 0
+badreceiveCounter = 0
+successCounter = 0
+
 while True:
     try:
-        data, addr = serverSock.recvfrom(32)
-    except OSError:
-        print('bad receive')
-    else:
-        chunk = b''
-        num = data.decode('utf-8')
+        while watching:
+            a = 'err '
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    watching = False
+                    break
+            try:
+                data, addr = serverSock.recvfrom(32)
+            except OSError:
+                badreceiveCounter += 1
+                # print('bad receive')
+                a += 'badRec '
+            else:
+                chunk = b''
+                num = data.decode('utf-8')
 
-        for j in range(int(num)):
-            data, addr = serverSock.recvfrom(64000)
-            chunk = chunk + data
-
-        with open('test.txt', 'rb') as binary_file:
-            OGchunk = binary_file.read()
-
-        if chunk == OGchunk:
-            print("They are the same")
-        else:
-            print("They are not the same")
-
-    '''
-    pygame.init()
-    pygame.display.set_caption('Rolt VNC')
-    screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
-    clock = pygame.time.Clock()
-    watching = True
-    
-    while watching:
-        print("here")
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                watching = False
-                break
-    
-        pixels = decompress(chunk)
-        # Create the Surface from raw pixels
-        img = pygame.image.fromstring(pixels, (WIDTH, HEIGHT), 'RGB')
-    
-        # Display the picture
-        screen.blit(img, (0, 0))
-        pygame.display.flip()
-        clock.tick(60)
-    '''
-
+                for j in range(int(num)):
+                    data, addr = serverSock.recvfrom(64000)
+                    chunk = chunk + data
+                try:
+                    pixels = zlib.decompress(chunk)
+                    # Create the Surface from raw pixels
+                    img = pygame.image.fromstring(pixels, (WIDTH, HEIGHT), 'RGB')
+                    # Display the picture
+                    screen.blit(img, (0, 0))
+                    pygame.display.flip()
+                    clock.tick(60)
+                    successCounter += 1
+                    print("Success!")
+                except zlib.error:
+                    decompressCounter += 1
+                    # print("decompress error")
+                    a += 'decomp '
+            if a != 'err ':
+                print(a)
+    finally:
+        serverSock.close()
+        print('Decompress Error = ' + str(decompressCounter))
+        print('Bad Receive Error = ' + str(badreceiveCounter))
+        print('Successes = ' + str(successCounter))
+        break
 
 
--- testUDPclient.py
+++ testUDPclient.py
@@ -4,32 +4,31 @@ from screenshot import ScreenShotObj
 
 UDP_IP_ADDRESS = "127.0.0.1"
 UDP_PORT_NO = 6789
-Message = 'hello test'
+
+sent = 0
 
 while True:
     SS = ScreenShotObj()
     chunk = SS.takescreenshot()
     with open('test.txt', 'wb') as binary_file:
         binary_file.write(chunk)
-    print(len(chunk))
+    # print(len(chunk))
     clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     clientSock.sendto((str(math.ceil((len(chunk) / 64000))).encode('utf-8')), (UDP_IP_ADDRESS, UDP_PORT_NO))
-    print(math.ceil((len(chunk) / 64000)))
+    # print(math.ceil((len(chunk) / 64000)))
     holder = 64000
     holder2 = 128000
+    sent += 1
     for i in range(int(math.ceil((len(chunk)/64000)))):
         if i == 0:
             clientSock.sendto(chunk[:holder], (UDP_IP_ADDRESS, UDP_PORT_NO))
-            print(i)
         elif i == int(math.ceil((len(chunk)/64000))) - 1:
             clientSock.sendto(chunk[holder:], (UDP_IP_ADDRESS, UDP_PORT_NO))
-            print(i)
             break
         else:
             clientSock.sendto(chunk[holder:holder2], (UDP_IP_ADDRESS, UDP_PORT_NO))
             holder = holder2
             holder2 = holder2 + 64000
-            print(i)
 
     # clientSock.sendto(b'fin'), (UDP_IP_ADDRESS, UDP_PORT_NO)
-
+    print(sent)
