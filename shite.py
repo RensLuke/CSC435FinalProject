d03e630f1cbd79377eb29d1457896c94369a5434
James Holt <jwholt3@gmail.com>
2019-04-11 13:08:36 -0500

    update no socket test file

--- shite.py
+++ shite.py
@@ -1,17 +1,11 @@
 import zlib
-import socket
 from screenshot import ScreenShotObj
 import pygame
-import threading
 
 UDP_IP_ADDRESS = "127.0.0.1"
 UDP_PORT_NO = 6789
 WIDTH = 1920
 HEIGHT = 1080
-buffer = []
-buffer_counter = 0
-decompressCounter = 0
-badreceiveCounter = 0
 successCounter = 0
 
 print("Starting")
@@ -28,25 +22,22 @@ while True:
 
             SS = ScreenShotObj()
             chunk = SS.takescreenshot()
-            # buff = threading.Thread(target=packet_buffer())
-            # buff.start()
 
             pixels = zlib.decompress(chunk)
-            # pixels = chunk
-            # pixels = zlib.decompress(buffer[co])
+
             # Create the Surface from raw pixels
             img = pygame.image.fromstring(pixels, (WIDTH, HEIGHT), 'RGB')
             # Display the picture
             screen.blit(img, (0, 0))
             pygame.display.flip()
-            # clock.tick(60)
+            clock.tick(60)
+
+            pygame.event.get()
             successCounter += 1
             co += 1
             print('co = ', co)
 
     finally:
-        print('Decompress Error = ' + str(decompressCounter))
-        print('Bad Receive Error = ' + str(badreceiveCounter))
         print('Successes = ' + str(successCounter))
         break
 
