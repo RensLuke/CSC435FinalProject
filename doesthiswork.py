d605e87f594af31896ce97256282bd3003703b19
LukeRens-Desktop <rensld@jacks.sfasu.edu>
2019-04-10 20:11:20 -0500

    forgot to push this

--- doesthiswork.py
+++ doesthiswork.py
@@ -2,47 +2,62 @@ import zlib
 import socket
 from screenshot import ScreenShotObj
 import pygame
+import threading
+
 
 UDP_IP_ADDRESS = "127.0.0.1"
 UDP_PORT_NO = 6789
 WIDTH = 1920
 HEIGHT = 1080
+buffer = []
+buffer_counter = 0
+decompressCounter = 0
+badreceiveCounter = 0
+successCounter = 0
+
+
+def packet_buffer():
+    global buffer_counter
+    p = 0
+    while buffer_counter != 500:
+        try:
+            data, addr = serverSock.recvfrom(32)
+            num = data.decode('utf-8')
+        except OSError:
+            print('bad receive')
+        else:
+            chunk = b''
+            for j in range(int(num)):
+                data, addr = serverSock.recvfrom(64000)
+                chunk = chunk + data
+            buffer.append(chunk)
+            buffer_counter += 1
+            print(buffer_counter)
+
 
 serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
-
+print("Starting")
 pygame.init()
 pygame.display.set_caption('Rolt VNC')
 screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 clock = pygame.time.Clock()
 watching = True
-decompressCounter = 0
-badreceiveCounter = 0
-successCounter = 0
 
+co = 0
 while True:
     try:
         while watching:
-            a = 'err '
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     watching = False
                     break
-            try:
-                data, addr = serverSock.recvfrom(32)
-            except OSError:
-                badreceiveCounter += 1
-                # print('bad receive')
-                a += 'badRec '
+            
             else:
-                chunk = b''
-                num = data.decode('utf-8')
-
-                for j in range(int(num)):
-                    data, addr = serverSock.recvfrom(64000)
-                    chunk = chunk + data
+                buff = threading.Thread(target=packet_buffer())
+                buff.start()
                 try:
-                    pixels = zlib.decompress(chunk)
+                    pixels = zlib.decompress(buffer[co])
                     # Create the Surface from raw pixels
                     img = pygame.image.fromstring(pixels, (WIDTH, HEIGHT), 'RGB')
                     # Display the picture
@@ -50,13 +65,14 @@ while True:
                     pygame.display.flip()
                     clock.tick(60)
                     successCounter += 1
-                    print("Success!")
+                    co += 1
+                    print('co = ', co)
+                    if co == buffer_counter:
+                        break
+                    # print("Success!")
                 except zlib.error:
                     decompressCounter += 1
-                    # print("decompress error")
-                    a += 'decomp '
-            if a != 'err ':
-                print(a)
+                    print("decompress error")
     finally:
         serverSock.close()
         print('Decompress Error = ' + str(decompressCounter))
