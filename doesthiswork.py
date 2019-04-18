e254029bbb899a57adb81d462227b9ec5a2cabff
Luke Rens <31190219+RensLuke@users.noreply.github.com>
2019-04-18 14:18:19 -0500

    Update doesthiswork.py
    best working copy as of now

--- doesthiswork.py
+++ doesthiswork.py
@@ -4,37 +4,16 @@ from screenshot import ScreenShotObj
 import pygame
 import threading
 
-
-UDP_IP_ADDRESS = "127.0.0.1"
+UDP_IP_ADDRESS = "0.0.0.0"
 UDP_PORT_NO = 6789
 WIDTH = 1920
 HEIGHT = 1080
 buffer = []
-buffer_counter = 0
-decompressCounter = 0
-badreceiveCounter = 0
+OSErrorCount = 0
+CompressionErrorCount = 0
+pygameErrorCount = 0
 successCounter = 0
 
-'''
-def packet_buffer():
-    global buffer_counter
-    p = 0
-    while buffer_counter != 500:
-        try:
-            data, addr = serverSock.recvfrom(32)
-            num = data.decode('utf-8')
-        except OSError:
-            print('bad receive')
-        else:
-            chunk = b''
-            for j in range(int(num)):
-                data, addr = serverSock.recvfrom(64000)
-                chunk = chunk + data
-            buffer.append(chunk)
-            buffer_counter += 1
-            print(buffer_counter)
-            pygame.event.get()
-'''
 
 serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
@@ -47,48 +26,47 @@ watching = True
 
 co = 0
 while True:
-    try:
         while watching:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     watching = False
                     break
-            
             else:
                 try:
                     data, addr = serverSock.recvfrom(32)
                     num = data.decode('utf-8')
+                    print(num)
                 except OSError:
-                    print('bad receive')
+                    print('Bad Receive')
+                    OSErrorCount += 1
                 else:
                     chunk = b''
                     for j in range(int(num)):
                         data, addr = serverSock.recvfrom(64000)
                         chunk = chunk + data
-                # buff = threading.Thread(target=packet_buffer())
-                # buff.start()
-                try:
-                    pixels = zlib.decompress(chunk)
-                    # Create the Surface from raw pixels
-                    img = pygame.image.fromstring(pixels, (WIDTH, HEIGHT), 'RGB')
-                    # Display the picture
-                    screen.blit(img, (0, 0))
-                    pygame.display.flip()
-                    clock.tick(60)
-                    successCounter += 1
-                    co += 1
-                    print('co = ', co)
-                    if co == buffer_counter:
-                        break
-                    # print("Success!")
-                except zlib.error:
-                    decompressCounter += 1
-                    print("decompress error")
-    finally:
-        serverSock.close()
-        print('Decompress Error = ' + str(decompressCounter))
-        print('Bad Receive Error = ' + str(badreceiveCounter))
-        print('Successes = ' + str(successCounter))
+                    try:
+                        zobj = zlib.decompressobj()
+                        pixels = zobj.decompress(chunk)
+                        print(len(pixels))
+                    except zlib.error:
+                        print("Compression Error")
+                        CompressionErrorCount += 1
+                    else:
+                        # Create the Surface from raw pixels
+                        try:
+                            img = pygame.image.fromstring(pixels, (WIDTH, HEIGHT), 'RGB')
+                        except ValueError:
+                            print("pygame error")
+                            pygameErrorCount += 1
+                        else:
+                            # Display the picture
+                            screen.blit(img, (0, 0))
+                            pygame.display.flip()
+                            clock.tick(60)
+                            successCounter += 1
+                            print("Success!")
         break
-
-
+print("bad receive = ", OSErrorCount)
+print("compression errors = ", CompressionErrorCount)
+print("pygame errors = ", pygameErrorCount)
+print("successes = ", successCounter)
