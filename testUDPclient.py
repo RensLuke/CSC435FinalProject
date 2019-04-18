995fcd59578b6dc3ae8c3e7ef9d1b11aa7502b1f
LukeRens-Desktop <rensld@jacks.sfasu.edu>
2019-04-18 14:08:22 -0500

    this is the best working copy as of now

--- testUDPclient.py
+++ testUDPclient.py
@@ -2,33 +2,31 @@ import socket
 import math
 from screenshot import ScreenShotObj
 
-UDP_IP_ADDRESS = "127.0.0.1"
+UDP_IP_ADDRESS = "192.168.86.36"
 UDP_PORT_NO = 6789
-
+segment_size = 64000
 sent = 0
 
 while True:
     SS = ScreenShotObj()
     chunk = SS.takescreenshot()
-    with open('test.txt', 'wb') as binary_file:
-        binary_file.write(chunk)
     # print(len(chunk))
     clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    clientSock.sendto((str(math.ceil((len(chunk) / 64000))).encode('utf-8')), (UDP_IP_ADDRESS, UDP_PORT_NO))
+    clientSock.sendto((str(math.ceil((len(chunk) / segment_size))).encode('utf-8')), (UDP_IP_ADDRESS, UDP_PORT_NO))
     # print(math.ceil((len(chunk) / 64000)))
-    holder = 64000
-    holder2 = 128000
+    holder = segment_size
+    holder2 = segment_size + segment_size
     sent += 1
-    for i in range(int(math.ceil((len(chunk)/64000)))):
+    for i in range(int(math.ceil((len(chunk)/segment_size)))):
         if i == 0:
             clientSock.sendto(chunk[:holder], (UDP_IP_ADDRESS, UDP_PORT_NO))
-        elif i == int(math.ceil((len(chunk)/64000))) - 1:
+        elif i == int(math.ceil((len(chunk)/segment_size))) - 1:
             clientSock.sendto(chunk[holder:], (UDP_IP_ADDRESS, UDP_PORT_NO))
             break
         else:
             clientSock.sendto(chunk[holder:holder2], (UDP_IP_ADDRESS, UDP_PORT_NO))
             holder = holder2
-            holder2 = holder2 + 64000
+            holder2 = holder2 + segment_size
 
     # clientSock.sendto(b'fin'), (UDP_IP_ADDRESS, UDP_PORT_NO)
     print(sent)
