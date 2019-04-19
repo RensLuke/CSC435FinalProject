4adc5fbe4e49ff6a5d4850c8c63e34e1bc999fe4
Luke Rens <31190219+RensLuke@users.noreply.github.com>
2019-04-19 10:40:23 -0500

    functional multicast as of 10:40am 4/19/2019

--- server.py
+++ server.py
@@ -7,14 +7,12 @@ UDP_PORT_NO = 6789
 segment_size = 64000
 sent = 0
 
-clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-clientSock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton('192.168.86.36'))
-
 while True:
     SS = ScreenShotObj()
     chunk = SS.takescreenshot()
     # print(len(chunk))
-    # clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    clientSock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton('192.168.86.36'))
     clientSock.sendto((str(math.ceil((len(chunk) / segment_size))).encode('utf-8')), (UDP_IP_ADDRESS, UDP_PORT_NO))
     # print(math.ceil((len(chunk) / 64000)))
     holder = segment_size
