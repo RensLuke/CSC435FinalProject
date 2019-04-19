2948b30da25e1a42d12e153988d9710c98d2cd46
Luke Rens <31190219+RensLuke@users.noreply.github.com>
2019-04-19 01:10:13 -0500

    Update server.py
    with working multicast

--- server.py
+++ server.py
@@ -2,16 +2,19 @@ import socket
 import math
 from screenshot import ScreenShotObj
 
-UDP_IP_ADDRESS = "192.168.86.36"
+UDP_IP_ADDRESS = '239.1.1.1'
 UDP_PORT_NO = 6789
 segment_size = 64000
 sent = 0
+
+clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+clientSock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton('192.168.86.36'))
 
 while True:
     SS = ScreenShotObj()
     chunk = SS.takescreenshot()
     # print(len(chunk))
-    clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     clientSock.sendto((str(math.ceil((len(chunk) / segment_size))).encode('utf-8')), (UDP_IP_ADDRESS, UDP_PORT_NO))
     # print(math.ceil((len(chunk) / 64000)))
     holder = segment_size
