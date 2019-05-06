c17b1044579fe7d76c4690b37cf4ad56590f2788
James Holt <jwholt3@gmail.com>
2019-05-06 01:11:35 -0500

    Update main for the GUI

--- Main.py
+++ Main.py
@@ -1,34 +1,70 @@
-import zlib
-import socket
-import struct
-from screenshot import ScreenShotObj
-import pygame
 from Main_Menu import Ui_dlgMain
+from Settings_Menu import Ui_dlgSettings
 from PyQt5 import QtCore, QtGui, QtWidgets
-import threading
 import sys
+
+ip = 'temp'
+multicast = 'temp'
+resolution = 'temp'
+
+
+class StartSettings(Ui_dlgSettings):
+
+    def __init__(self, window2):
+        Ui_dlgSettings.__init__(self)
+        self.setupUi(window2)
+        self.init_ui()
+
+    def init_ui(self):
+        self.txtIP.setText('127.0.0.1')  # TODO: change this to be the SFA IP set
+        # self.buttonBox.accepted.connect(self.save_settings())
+        # self.buttonBox.rejected.connect(self.cancel())
+        print('settings opened')
+
+    def save_settings(self):
+        global ip, multicast, resolution
+        ip = self.txtIP.text().strip()
+        multicast = self.cmbMulticast.currentText()
+        resolution = self.cmbResolution.currentText()
+        print('settings saved')
+
+    def cancel(self):
+        print('canceled without saving')
 
 
 class StartUp(Ui_dlgMain):
 
-    def __init__(self, dialog):
+    def __init__(self, window):
         Ui_dlgMain.__init__(self)
-        self.setupUi(dialog)
-        # Ui_dlgMain.btnStart
-        self.btnStart.clicked.connect(self.start_client())
-        self.btnExit.clicked.connect(self.exit())
+        self.setupUi(window)
+        self.init_ui()
 
+    def init_ui(self):
+        # self.btnStart.clicked.connect(self.start())
+        # self.btnExit.clicked.connect(self.exit())
+        # self.btnSettings.clicked.connect(self.settings())
+        print('main menu')
 
-
-    def start_client(self):
-        print('IT WORKED')
+    def start(self):
+        print('Starting')
+        # TODO: Put the client/server code here in each file
 
     def exit(self):
         print('Exiting')
         sys.exit()
 
+    def settings(self):
+        # Opens the settings dialog
+        app2 = QtWidgets.QApplication(sys.argv)
+        dialog2 = QtWidgets.QDialog()
+        prog2 = StartSettings(dialog2)
+
+        dialog2.show()
+        sys.exit(app2.exec_())
+
 
 if __name__ == '__main__':
+    # Opens the main dialog
     app = QtWidgets.QApplication(sys.argv)
     dialog = QtWidgets.QDialog()
     prog = StartUp(dialog)
