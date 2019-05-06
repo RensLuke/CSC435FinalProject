1b8ff67524a13dd52c9db5c58066f6adc44bd840
James Holt <jwholt3@gmail.com>
2019-05-06 14:41:18 -0500

    Add JCProjector

---
+++ JCProjector.py
@@ -0,0 +1,81 @@
+from Main_Menu import Ui_dlgMain
+from Settings_Menu import Ui_dlgSettings
+from PyQt5 import QtWidgets
+import sys
+import server
+import helper
+
+
+ip = 'temp'
+multicast = 'temp'
+resolution = 'temp'
+saved = False
+
+
+class StartSettings(Ui_dlgSettings):
+
+    def __init__(self, window2):
+        Ui_dlgSettings.__init__(self)
+        self.setupUi(window2)
+        self.init_ui()
+
+    def init_ui(self):
+        # Initialize settings menu
+        network = helper.NetworkingObj()
+        self.txtIP.setText(network.get_sfa_ip())  # TODO: change this to be the SFA IP set
+        self.buttonBox.accepted.connect(lambda: self.save_settings())
+        self.buttonBox.rejected.connect(lambda: self.cancel())
+        print('settings opened')
+
+    def save_settings(self):
+        global ip, multicast, resolution, saved
+        ip = self.txtIP.text().strip()
+        multicast = self.cmbMulticast.currentText()
+        resolution = self.cmbResolution.currentText()
+        saved = True
+        print('settings saved')
+
+    def cancel(self):
+        print('canceled without saving')
+
+
+class StartUp(Ui_dlgMain):
+
+    def __init__(self, window):
+        Ui_dlgMain.__init__(self)
+        self.setupUi(window)
+        self.init_ui()
+
+    def init_ui(self):
+        # Initialize main menu
+        self.btnStart.clicked.connect(lambda: self.start())
+        self.btnExit.clicked.connect(lambda: self.exit())
+        self.btnSettings.clicked.connect(lambda: self.settings())
+        print('main menu')
+
+    def start(self):
+        # Start Client/Server respectively
+        print('Starting')
+        server.run_me(ip, multicast, resolution, saved)
+
+    def exit(self):
+        print('Exiting')
+        sys.exit()
+
+    def settings(self):
+        # Opens the settings dialog
+        global prog2
+        self.dialog2 = QtWidgets.QDialog()
+        prog2 = StartSettings(self.dialog2)
+        self.dialog2.show()
+
+
+if __name__ == '__main__':
+    # Opens the main dialog
+    app = QtWidgets.QApplication(sys.argv)
+    dialog = QtWidgets.QDialog()
+    prog = StartUp(dialog)
+
+    dialog.show()
+    sys.exit(app.exec_())
+
--- server.py
+++ server.py
@@ -2,42 +2,49 @@ import socket
 import math
 import helper
 
+# ip_addr = '192.168.0.14'  # For testing
+network = helper.NetworkingObj()
+ip_addr = network.get_sfa_ip()
+multicast_addr = '239.1.1.1'
+resolution = '1920 x 1080'
 
-def get_sfa_ip():
-    ipList = socket.gethostbyname_ex(socket.gethostname())[2]
-    for x in range(len(ipList)):
-        if '144.96' in ipList[x]:
-            return ipList[x]
+
+def run_me(pIP, pMulti, pRes, pSaved):
+    global ip_addr, multicast_addr, resolution
+    if pSaved:
+        ip_addr = pIP
+        multicast_addr = pMulti
+        resolution = pRes
+    start()
 
 
-UDP_IP_ADDRESS = '239.1.1.1'
-UDP_PORT_NO = 6789
-segment_size = 64000
-sent = 0
-my_ip = get_sfa_ip()
-# my_ip = '192.168.0.14'  # For testing
+def start():
+    UDP_IP_ADDRESS = multicast_addr
+    UDP_PORT_NO = 6789
+    segment_size = 64000
+    sent = 0
 
-while True:
-    SS = helper.ScreenShotObj()
-    chunk = SS.takescreenshot()
-    # print(len(chunk))
-    clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    clientSock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(my_ip))
-    clientSock.sendto((str(math.ceil((len(chunk) / segment_size))).encode('utf-8')), (UDP_IP_ADDRESS, UDP_PORT_NO))
-    # print(math.ceil((len(chunk) / 64000)))
-    holder = segment_size
-    holder2 = segment_size + segment_size
-    sent += 1
-    for i in range(int(math.ceil((len(chunk)/segment_size)))):
-        if i == 0:
-            clientSock.sendto(chunk[:holder], (UDP_IP_ADDRESS, UDP_PORT_NO))
-        elif i == int(math.ceil((len(chunk)/segment_size))) - 1:
-            clientSock.sendto(chunk[holder:], (UDP_IP_ADDRESS, UDP_PORT_NO))
-            break
-        else:
-            clientSock.sendto(chunk[holder:holder2], (UDP_IP_ADDRESS, UDP_PORT_NO))
-            holder = holder2
-            holder2 = holder2 + segment_size
+    while True:
+        SS = helper.ScreenShotObj()
+        chunk = SS.takescreenshot()
+        # print(len(chunk))
+        clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        clientSock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(ip_addr))
+        clientSock.sendto((str(math.ceil((len(chunk) / segment_size))).encode('utf-8')), (UDP_IP_ADDRESS, UDP_PORT_NO))
+        # print(math.ceil((len(chunk) / 64000)))
+        holder = segment_size
+        holder2 = segment_size + segment_size
+        sent += 1
+        for i in range(int(math.ceil((len(chunk)/segment_size)))):
+            if i == 0:
+                clientSock.sendto(chunk[:holder], (UDP_IP_ADDRESS, UDP_PORT_NO))
+            elif i == int(math.ceil((len(chunk)/segment_size))) - 1:
+                clientSock.sendto(chunk[holder:], (UDP_IP_ADDRESS, UDP_PORT_NO))
+                break
+            else:
+                clientSock.sendto(chunk[holder:holder2], (UDP_IP_ADDRESS, UDP_PORT_NO))
+                holder = holder2
+                holder2 = holder2 + segment_size
 
-    # clientSock.sendto(b'fin'), (UDP_IP_ADDRESS, UDP_PORT_NO)
-    print(sent)
+        # clientSock.sendto(b'fin'), (UDP_IP_ADDRESS, UDP_PORT_NO)
+        print(sent)
