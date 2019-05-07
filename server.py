ece122409986f3fc2bcf3cd2610b3838e4a1dd60
Luke Rens <31190219+RensLuke@users.noreply.github.com>
2019-05-06 20:19:12 -0500

    added comments

--- server.py
+++ server.py
@@ -19,6 +19,7 @@ def run_me(pIP, pMulti, pRes, pSaved):
 
 
 def start():
+    # Defining variables
     global ip_addr, multicast_addr, resolution
 
     UDP_IP_ADDRESS = multicast_addr
@@ -30,26 +31,28 @@ def start():
     height = int(resolution[2])
 
     while True:
+        # Declaring screenshot object
         SS = helper.ScreenShotObj()
         chunk = SS.takescreenshot(width, height)
-        # print(len(chunk))
-        clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        clientSock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(ip_addr))
-        clientSock.sendto((str(math.ceil((len(chunk) / segment_size))).encode('utf-8')), (UDP_IP_ADDRESS, UDP_PORT_NO))
-        # print(math.ceil((len(chunk) / 64000)))
+
+        # Setting up socket
+        clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # Declaring UDP socket
+        clientSock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(ip_addr))  # Telling kernel to send packets as multicast
+        clientSock.sendto((str(math.ceil((len(chunk) / segment_size))).encode('utf-8')), (UDP_IP_ADDRESS, UDP_PORT_NO))  #  finding/sending how many chunks to send
+        
+        # declaring holder objects for chunking
         holder = segment_size
         holder2 = segment_size + segment_size
         sent += 1
-        for i in range(int(math.ceil((len(chunk)/segment_size)))):
+        for i in range(int(math.ceil((len(chunk)/segment_size)))):  # iterating i many chunks
             if i == 0:
-                clientSock.sendto(chunk[:holder], (UDP_IP_ADDRESS, UDP_PORT_NO))
-            elif i == int(math.ceil((len(chunk)/segment_size))) - 1:
+                clientSock.sendto(chunk[:holder], (UDP_IP_ADDRESS, UDP_PORT_NO))  # sending first chunk
+            elif i == int(math.ceil((len(chunk)/segment_size))) - 1:  # sending last chunk
                 clientSock.sendto(chunk[holder:], (UDP_IP_ADDRESS, UDP_PORT_NO))
                 break
             else:
-                clientSock.sendto(chunk[holder:holder2], (UDP_IP_ADDRESS, UDP_PORT_NO))
+                clientSock.sendto(chunk[holder:holder2], (UDP_IP_ADDRESS, UDP_PORT_NO)) # sending all the chunks inbetween
                 holder = holder2
                 holder2 = holder2 + segment_size
 
-        # clientSock.sendto(b'fin'), (UDP_IP_ADDRESS, UDP_PORT_NO)
         print(sent)
